ecd85a5c04a2eb409f0f39f3227bcbaa953c3a84
CorvusGlaive <neos34115@gmail.com>
2020-04-02 17:49:19 +0400

    widget clock change appearance

--- .config/awesome/widget/clock.lua
+++ .config/awesome/widget/clock.lua
@@ -4,11 +4,12 @@ local gears = require("gears")
 local beautiful = require("beautiful")
 
 
-local mytextclock = wibox.widget.textclock('<span font="Rubik Medium 10">%a %b %d, </span> <span font="Rubik Bold 11">%H:%M</span>')
+local mytextclock = wibox.widget.textclock('%a %b %d, <b>%H:%M</b>')
+mytextclock.font = 'SF Pro Display 11'
 local month_calendar = awful.widget.calendar_popup.month {
     -- long_weekdays = true,
     bg = beautiful.top_panel_bg,
-    font = "Rubik Medium 10",
+    font = 'Inter SemiBold 10',
     style_month = {
         bg_color = "#00000000",
         border_width = 0,
@@ -27,7 +28,13 @@ local month_calendar = awful.widget.calendar_popup.month {
         border_width = 0,
     },
     style_focus = {
+        bg_color = "#408cdd",
         border_width = 0,
+        markup = function(t) return t end,
+        shape = function(cr,w,h)
+              gears.shape.rounded_rect(cr, w, h, 4)
+          end,
+        padding = dpi(3)
     },
 }
 month_calendar:attach( mytextclock, "tc")
