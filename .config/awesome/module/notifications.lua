150289f378ce8849fabfa1b1745ab7213567c49a
CorvusGlaive <neos34115@gmail.com>
2020-04-02 17:49:02 +0400

    notify change appearance

--- .config/awesome/module/notifications.lua
+++ .config/awesome/module/notifications.lua
@@ -1,21 +1,23 @@
 local naughty = require('naughty')
 local beautiful = require('beautiful')
 local gears = require('gears')
+local awful = require('awful')
 
 -- Naughty presets
 naughty.config.padding = 8
 naughty.config.spacing = 8
 
 naughty.config.defaults.timeout = 5
-naughty.config.defaults.screen = 1
-naughty.config.defaults.position = 'bottom_left'
+naughty.config.defaults.screen = awful.screen.focused()
+naughty.config.defaults.position = 'top_right'
 naughty.config.defaults.margin = dpi(16)
 naughty.config.defaults.ontop = true
-naughty.config.defaults.font = 'Roboto Regular 10'
+naughty.config.defaults.font = 'Inter Medium 10'
 naughty.config.defaults.icon = nil
 naughty.config.defaults.icon_size = dpi(32)
 naughty.config.defaults.shape = gears.shape.rounded_rect
-naughty.config.defaults.border_width = 0
+naughty.config.defaults.border_width = 1
+naughty.config.defaults.border_color = "#6b6b6b"
 naughty.config.defaults.hover_timeout = nil
 
 -- Error handling
