62687fe0955b764734ce346f635dd9758c8953dc
Alex Nas <neos34115@gmail.com>
2021-10-06 17:38:10 +0400

    nvim>nvim-tree: Move settings to setup

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,11 +1,19 @@
 vim.g.nvim_tree_hide_dotfiles = 0
 vim.g.nvim_tree_indent_markers = 0
-vim.g.nvim_tree_follow = 1
-vim.g.nvim_tree_auto_open = 1
 vim.g.nvim_tree_ignore = { ".git", "node_modules" }
 
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
-vim.g.nvim_tree_bindings = {
-  { key = "l", cb = tree_cb("edit") },
-  { key = "h", cb = tree_cb("close_node") },
+
+require("nvim-tree").setup {
+  open_on_setup = true,
+  update_focused_file = { enable = true },
+  view = {
+    auto_resize = true,
+    mappings = {
+      list = {
+        { key = "l", cb = tree_cb("edit") },
+        { key = "h", cb = tree_cb("close_node") },
+      },
+    },
+  },
 }
